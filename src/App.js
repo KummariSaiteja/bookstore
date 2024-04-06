3dedac1d5b2e2d666ea590374335ebf7cb01e887
KummariSaiteja <saiteja@lyrostech.com>
2024-04-06 16:00:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [books, setBooks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    
+    const fetchBooks = async () => {
+      try {
+        const response = await axios.get(
+          `https://www.googleapis.com/books/v1/volumes?q=harry+potter`
+        );
+        setBooks(response.data.items);
+      } catch (error) {
+        console.error('Error fetching books:', error);
+      }
+    };
+
+    fetchBooks();
+  }, []);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${searchQuery}`
+      );
+      setBooks(response.data.items);
+    } catch (error) {
+      console.error('Error searching books:', error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1>Virtual Bookstore</h1>
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchQuery}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleSearch}>Search</button>
+        </div>
       </header>
+      <main>
+        <div className="book-list">
+          {books.map((book) => (
+            <div key={book.id} className="book-card">
+              <img
+                src={book.volumeInfo.imageLinks?.thumbnail}
+                alt={book.volumeInfo.title}
+              />
+              <h3>{book.volumeInfo.title}</h3>
+              <p>{book.volumeInfo.authors?.join(', ')}</p>
+              <button>More Info</button>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
